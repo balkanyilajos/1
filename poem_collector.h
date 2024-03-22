741a36e101d028a055e39bf6460d3fe73b80e85c
Balkányi Lajos <balkanyilajos@gmail.com>
2024-03-22 18:17:51 +0100

    deleted unnecessary files

---
+++ poem_collector.h
@@ -0,0 +1,21 @@
+#ifndef POEM_COLLECTOR_H
+#define POEM_COLLECTOR_H
+
+#include "poem.h"
+#include "poem_container.c"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void run(const char* filename);
+Poem* readPoemFromConsole(void);
+int readNumberFromConsole(void);
+char* readStringFromConsole(char end);
+void trim(char* str);
+
+void MemoryAllocationException(void);
+void IOException(void);
+
+#endif
